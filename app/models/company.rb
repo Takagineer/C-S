1bc7f49aebbcc0933fbb689cf80a04e4a2cdfba9
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-02-12 20:33:01 +0900

    companyモデル作成

---
+++ app/models/company.rb
@@ -0,0 +1,14 @@
+class Company < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :email
+    validates :password
+    validates :company_name
+    validates :company_introduction
+
+  end
+end
