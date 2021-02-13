9b62824bc56b80590349ca4f677fd2f69631a581
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-02-13 18:28:20 +0900

    deviseボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :students
-  # devise_for :companies
+  devise_for :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "businesses#index"
   resources :businesses, Only: [:index]
