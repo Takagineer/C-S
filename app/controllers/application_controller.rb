b402afd8d63a3f58d6ba7a8176500871c3bfedf6
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-02-14 15:41:50 +0900

    学生側の新規登録機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  bofere_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name, :company_introduction, :university, :first_name, :last_name, :student_introduction, :skill, :age, :year])
+    end
+
 end
