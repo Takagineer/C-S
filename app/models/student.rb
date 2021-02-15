3768fa924c9e6acf64a36b5cecafd03fed1b195f
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-02-15 14:34:34 +0900

    devise/マイグレーションファイルの記述修正

--- app/models/student.rb
+++ app/models/student.rb
@@ -4,16 +4,16 @@ class Student < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         with_options presence: true do
-          validates :email
-          validates :password
-          validates :university
-          validates :first_name
-          validates :last_name
-          validates :student_introduction
-          validates :skill
-          validates :age
-          validates :year
-        end
+  with_options presence: true do
+    validates :email
+    validates :password
+    validates :university
+    validates :first_name
+    validates :last_name
+    validates :student_introduction
+    validates :skill
+    validates :age
+    validates :year
+  end
 
 end
