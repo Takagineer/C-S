00404afd7ffd51abd17d1ecd6f664cc49f4c5eb5
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-02-23 15:39:30 +0900

    業務検索機能

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,14 @@
 class StudentsController < ApplicationController
   def index
   end
+
+  def search
+    @student_search_businesses = Business.search(params[:keyword])
+  end
+
+
+  private
+    def student_search_businesses
+      
+    end
 end
