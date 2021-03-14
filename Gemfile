d6d1e22cdd78f95a01250bb61aa6075a0d91b3af
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-14 21:35:50 +0900

    contracted_work_model_description

--- Gemfile
+++ Gemfile
@@ -58,5 +58,3 @@ gem 'pry-rails'
 
 gem 'mini_magick'
 gem 'image_processing', '~>1.2'
-
-gem 'acts_as_follower', github: 'tcocca/acts_as_follower'
--- app/models/contracted_work.rb
+++ app/models/contracted_work.rb
@@ -1,4 +1,9 @@
 class ContractedWork < ApplicationRecord
   belongs_to :student
   belongs_to :business
+
+  with_options presence: true do
+    business_id
+    student_id
+  end
 end
