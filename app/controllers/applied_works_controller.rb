52a7f8301a620e58780ba4471a50bd191e79d9c9
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-16 21:16:58 +0900

    applied_works_create_action

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -1,2 +1,6 @@
 class AppliedWorksController < ApplicationController
+  def create
+    @applied_works = AppliedWork.create(business_id:@business.id, student_id:current_student.id, status:1)
+    
+  end
 end
