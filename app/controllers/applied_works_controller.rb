b050477ebf23df5530fa4ad6c8246f6ba87802c3
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-17 18:11:39 +0900

    applied_works_redirect

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -3,5 +3,6 @@ before_action :authenticate_student!, only:[:create]
 
   def create
     @applied_works = AppliedWork.create(business_id:@business.id, student_id:current_student.id)
+    redirect to  root_path
   end
 end
