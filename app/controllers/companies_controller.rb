bab7ff56c6e583098ba76e4c2d59afcb6e8678ea
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-19 08:47:34 +0900

    company_index_recruiting_business

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,6 +1,6 @@
 class CompaniesController < ApplicationController
   def index
-    @company_businesses = Business.where(company_id = params[:id])
+    @company_businesses = Business.where(company_id: current_company.id)
   end
   
   def show
