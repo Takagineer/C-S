21c504bb54386c45786a9aa4ace196769573b342
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-22 20:58:19 +0900

    fix_link_to_applied_work

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -2,6 +2,10 @@ class AppliedWorksController < ApplicationController
 before_action :authenticate_student!, only:[:create]
 before_action :authenticate_company!, only:[:index]
 
+  def index
+    
+  end
+
   def create
     @applied_works = AppliedWork.create(business_id: params[:business_id], student_id:current_student.id, status:1)
     @applied_works.save
