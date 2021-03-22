abf1280cb2e63e5b21be0cd733ebdf7fdebe9b2d
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-03-22 15:43:13 +0900

    before_action_authenticate_company!

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -1,5 +1,6 @@
 class AppliedWorksController < ApplicationController
 before_action :authenticate_student!, only:[:create]
+before_action :authenticate_company!, only:[:index]
 
   def create
     @applied_works = AppliedWork.create(business_id: params[:business_id], student_id:current_student.id, status:1)
