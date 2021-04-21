2224d860d1c7ccea573768e786d81f8fd525e758
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-04-21 09:08:02 +0900

    applied_work_route_update

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -4,11 +4,15 @@ before_action :authenticate_company!, only:[:index]
 
   def index
     @applied_works = AppliedWork.where(business_id:params[:business_id])
+    @status = AppliedWork.new
   end
 
   def create
     @applied_works = AppliedWork.create(business_id: params[:business_id], student_id:current_student.id, status:1)
-    @applied_works.save
     redirect_to root_path
+  end
+
+  def update
+    
   end
 end
