74ff4df4a2ab47251f62fbe67d3f16c6530935ac
Takagineer <75421119+Takagineer@users.noreply.github.com>
2021-04-27 13:55:57 +0900

    fix_applied_works_controller

--- app/controllers/applied_works_controller.rb
+++ app/controllers/applied_works_controller.rb
@@ -1,18 +1,23 @@
 class AppliedWorksController < ApplicationController
 before_action :authenticate_student!, only:[:create]
-before_action :authenticate_company!, only:[:index]
+before_action :authenticate_company!, only:[:index, :update]
 
   def index
     @applied_works = AppliedWork.where(business_id:params[:business_id])
-    @status = AppliedWork.new
+    @statuses = @applied_works.select(:status)
   end
-
+  
   def create
     @applied_works = AppliedWork.create(business_id: params[:business_id], student_id:current_student.id, status:1)
     redirect_to root_path
   end
-
+  
   def update
-    
   end
+
+
+  private
+    def status_params 
+      params.require(:AppliedWork).permit(:status)
+    end
 end
